6ffe8bf83ca65ae267258196a8d660001fb064f3
awaseem2 <awaseem2@illinois.edu>
2021-03-10 23:14:11 -0600

    added starter code and modified script

--- web-init.sh
+++ web-init.sh
@@ -8,24 +8,23 @@ cd ..
 
 # create github repo
 gh repo create $1 --confirm --private
-cd $1
-
-# create index.html
-touch index.html
-
-# copy contents of index.html
-# TODO
 
 # create style folder
-mkdir style
+mkdir $1/style
 
-# create style.css inside style folder
-touch style/style.css
+# copy web code and read me
+cp web-init-script/exampleIndex.html $1/index.html
 
-# copy contents of style.css
-# TODO
+cp web-init-script/style/bootstrap.min.css $1/style
+
+cp web-init-script/style/exampleStyle.css $1/style/style.css
+
+cp web-init-script/ExampleREADME.md $1/README.md
+
+# cd into repo dir
+cd $1
 
 # commit and push
-git add index.html style/style.css
+git add -A
 git commit -m "Initialize repo"
 git push --set-upstream origin master
